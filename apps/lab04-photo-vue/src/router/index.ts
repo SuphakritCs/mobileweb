bd4a672ad653290ba33fddb2253d00ca3e8fde8f
SuphakritCs <suphakrit.san@kkumail.com>
2026-01-11 17:34:13 +0700

    fix all the time

--- apps/lab04-photo-vue/src/router/index.ts
+++ apps/lab04-photo-vue/src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHashHistory } from '@ionic/vue-router';
+
 import { RouteRecordRaw } from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
 
@@ -32,7 +33,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
